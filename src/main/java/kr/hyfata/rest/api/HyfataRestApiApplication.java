4466093171d590282b93db1155be8dccd2b1bb31
Najoan <68422289+najoan125@users.noreply.github.com>
2025-05-20 22:26:35 +0900

    initial commit

---
+++ src/main/java/kr/hyfata/rest/api/HyfataRestApiApplication.java
@@ -0,0 +1,13 @@
+package kr.hyfata.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyfataRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyfataRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/hyfata/rest/api/HyfataRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hyfata.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyfataRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
